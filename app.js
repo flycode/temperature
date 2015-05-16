7f8795ca4cd37c1fb24dd7cc2cc08c9bc187a16e
Nicolas Brizuela <nicobrizuela@tobiz-laptop.redmond.corp.microsoft.com>
2015-05-16 15:20:22 -0300

    First commit

--- app.js
+++ app.js
@@ -23,61 +23,11 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Email configuration
-var smtpTransport = nodemailer.createTransport("SMTP",{
-    service: "Gmail",
-    auth: {
-        user: "grayshirts.smtp@gmail.com",
-        pass: "grayshirts"
-    }
-});
-
 // Methods
 /* GET home page. */
 app.get('/', function(req, res) {
   res.render('index');
 });
-/* GET works page. */
-app.get('/work', function(req, res) {
-    res.render('work',{
-    section: 'work'
-  });
-});
-app.get('/work/:id', function(req, res) {
-    res.render('work/'+req.params.id,{
-    section: 'work'
-  });
-});
-/* GET about page. */
-app.get('/about', function(req, res) {
-  res.render('about',{
-    section: 'about'
-  });
-});
-/* GET contact page. */
-app.get('/contact', function(req, res) {
-  res.render('contact',{
-    section: 'contact'
-  });
-});
-/* POST contact page. */  
-app.post('/contact', function (req, res) {
-    var mailOptions = {
-        from: req.param('email', null), // sender address
-        to: 'nico@grayshirts.com.ar', // list of receivers
-        subject: 'Contacto Grayshirts', // Subject line
-        html: '<b>Name:</b> ' + req.param('name', null) + '<br><b>Email:</b> ' + req.param('email', null) + '<br><b>Company:</b> ' + req.param('company', null) + '<br><b>Project:</b> ' + req.param('project', null) + '<br><b>Message:</b> ' +  req.param('desc', null)
-    }
-    smtpTransport.sendMail(mailOptions, function(error, response){
-        if(error){
-          res.render("contact", { error: "Ah ocurrido un error. Intentalo en unos instantes." });
-        }else{
-          res.render("contact", { success: "Mensaje enviado." });
-        }
-        smtpTransport.close(); // shut down the connection pool, no more messages
-    });
-});
-
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
